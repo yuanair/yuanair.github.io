8cb296ac53c6ad1b6f20e9fb869f0faff740c982
air <2283106661@qq.com>
2025-06-27 13:30:55 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
-    base: './',
-    build: {
-        outDir: './dist',
-        assetsDir: './assets'
-    }
+    base: '/yuanair.github.io/',
+    build: {outDir: 'dist'}
 });
