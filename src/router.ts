401ba72740796d6333137c445a192b180d6dab87
air <2283106661@qq.com>
2025-06-27 12:44:13 +0800

    fix

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes
 });
 export default router;
