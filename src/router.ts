8435d24d2e730c0be2299f03f39d2adfbdceabab
air <2283106661@qq.com>
2025-06-27 12:07:39 +0800

    fix

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import {createRouter, createWebHashHistory, type RouteRecordRaw} from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-    {path: '/', name: 'Home', component: () => import('../views/Home.vue')},
-    {path: '/error-reboot', name: 'Error_Reboot', component: () => import('../views/Game-Error_Reboot.vue')}
+    {path: '/', name: 'Home', component: () => import('./views/Home.vue')},
+    {path: '/error-reboot', name: 'Error_Reboot', component: () => import('./views/Game-Error_Reboot.vue')}
 ];
 
 const router = createRouter({
